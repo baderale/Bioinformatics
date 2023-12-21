c44d80955e90a23ba79eae70a1cf39cf15ec3473
Bader Ale <bader.ale@fiu.edu>
2023-12-20 19:40:16 -0500

    Added DNAToolkit

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -2,4 +2,9 @@
 Nucleotides = ['A', 'C', 'T', 'G']
 
 # DNA Sequence Validation fucntion
-
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotides:
+            return False
+    return tmpseq
