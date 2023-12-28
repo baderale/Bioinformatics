cbb5e8b9c0745ece26cb2dc0bf74857420cb3a63
Bader Ale <bader.ale@fiu.edu>
2023-12-28 18:40:10 -0500

    update

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,27 +1,25 @@
 # DNA Toolset/Code 
 from structures import *
 
-# DNA Sequence Validation fucntion
 def validateSeq(dna_seq):
+    """DNA Sequence Validation function"""
     tmpseq = dna_seq.upper() 
     for nuc in tmpseq:
         if nuc not in Nucleotides:
             return False
     return tmpseq   # returns an uppercase version
 
-# Count number of nucleotides in a DNA sequence
 def countNucFrequency(seq):
+    """Count number of nucleotides in a DNA sequence"""
     tmpFreqDict = {'A':0, 'C':0, 'G':0, 'T':0}
     for nuc in seq:
         tmpFreqDict[nuc] += 1
     return tmpFreqDict
 
-# Create a complemantary DNA sequence, DNA--->RNA
 def transcription(seq):
     """DNA to RNA ---> Replaces Thymine(T) with Uracil(U)"""
     return seq.replace('T', 'U')
 
-# Create a DNA reverse complement
 def reverse_complement(seq):
     """ Swaps Adenine(A) with Thymine(T) and Guanine(G) with Cytosine(C).
         Creates a reverse string"""
@@ -30,3 +28,12 @@ def reverse_complement(seq):
 def gc_content(seq):
         """GC Content in a DNA/RNA sequence"""
         return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+def gc_content_subsec(seq, k=20):
+        """GC Content in a DNA/RNA sub-sequence length k. k=20 by default"""
+        res = []
+        for i in range(0, len(seq) - k + 1, k):
+            subseq = seq[i:i + k]
+            res.append(
+                round((subseq.count('C') + subseq.count('G')) / len(subseq) * 100))
+        return res
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from DNAToolkit import *
 from utilities import colored
 import random
 
+x = int(input(f'Input the number of nucleotides:'))
 randDNAStr = ''.join([random.choice(Nucleotides)
-                      for nuc in range(50)])
+                      for nuc in range(x)])
 
 DNAStr = validateSeq(randDNAStr) 
 
@@ -15,3 +16,4 @@ print(f"[4] *** DNA Sequence + Reverse Complement:\n        5' {colored(DNAStr)}
 print(f"           {''.join(['|' for c in range(len(DNAStr))])}")
 print(f"        3' {colored(reverse_complement(DNAStr))} 5'")
 print(f'[5] *** G/C Content: {gc_content(DNAStr)}%')
+print(f'[6] *** GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=20)}\n')
